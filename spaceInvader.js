248b1e9d9570d647f8d8dfe673a52a1422ba6be9
Dikra Bouabdellah <dikra.bouabdellah@student.ehb.be>
2022-10-09 18:04:44 +0200

    Drew my space invader

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,59 @@
 "use strict";
 
 alert("Draw your space invader here");
+
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+drawSpaceInvader();
+
+function drawSpaceInvader(){
+
+    //background black box
+
+    context.beginPath();
+    context.rect(100,100,300,300)
+    context.fill();
+
+    //eyes
+
+    context.fillStyle = "#AAFF00";
+    context.beginPath();
+    context.rect(175,125,50,100);
+    context.fill();
+
+    context.fillStyle = "#AAFF00";
+    context.beginPath();
+    context.rect(275,125,50,100);
+    context.fill();
+
+    //mouth
+
+    context.fillStyle = "#AAFF00";
+    context.beginPath();
+    context.rect(125,275,50,50);
+    context.fill();
+
+    context.fillStyle = "#AAFF00";
+    context.beginPath();
+    context.rect(175,325,50,50);
+    context.fill();
+
+    context.fillStyle = "#AAFF00";
+    context.beginPath();
+    context.rect(225,275,50,50);
+    context.fill();
+
+    context.fillStyle = "#AAFF00";
+    context.beginPath();
+    context.rect(275,325,50,50);
+    context.fill();
+
+    context.fillStyle = "#AAFF00";
+    context.beginPath();
+    context.rect(325,275,50,50);
+    context.fill();
+
+}
